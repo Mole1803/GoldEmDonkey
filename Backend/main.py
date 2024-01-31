69bafdb255e6bb4aa64c8e2541349cbcb4e8cc5b
moritzhartmann <mole-0@web.de>
2024-01-31 12:17:44 +0100

    added folderstructure

---
+++ Backend/main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+
+from _DatabaseCall import app
+
+
+if __name__ == '__main__':
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
