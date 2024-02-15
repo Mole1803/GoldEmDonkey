c9978f6ee885cb01bc57cf8701b75512dc4ad931
Trumen2010 <105265649+Trumen2010@users.noreply.github.com>
2024-02-15 08:15:00 +0100

    Add royal flush, straight flush, four of a kind allgorithms and unittests

--- Backend/Model/dto/Card.py
+++ Backend/Model/dto/Card.py
@@ -1,3 +1,10 @@
 class Card:
     colour: int
     value: int
+
+    def __init__(self, colour: int, value: int):
+        self.colour = colour
+        self.value = value
+
+    def __str__(self):
+        return str(self.colour) + ' ' + str(self.value)
