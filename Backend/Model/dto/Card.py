9ff61b14f53fab672231906f7148570f8b82afed
Trumen2010 <105265649+Trumen2010@users.noreply.github.com>
2024-03-01 09:15:52 +0100

    Edited Model to Database

--- Backend/Model/dto/Card.py
+++ Backend/Model/dto/Card.py
@@ -1,10 +1,12 @@
 class Card:
     colour: int
     value: int
+    id: int
 
-    def __init__(self, colour: int, value: int):
+    def __init__(self, colour: int, value: int, id=None):
         self.colour = colour
         self.value = value
+        self.id = id
 
     def __str__(self):
         return str(self.colour) + ' ' + str(self.value)
--- Backend/Model/dto/Player.py
+++ Backend/Model/dto/Player.py
@@ -1,5 +1,11 @@
 class Player:
-    cards: []
     chips: int
     name: str
     id: int
+    position: int
+
+    def __init__(self, chips, name, id, position):
+        self.chips = chips
+        self.name = name
+        self.id = id
+        self.position = position
